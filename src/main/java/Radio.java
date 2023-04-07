baf332d493b15f2ca351534799f1de048f90bedf
Ginnyelf <ginnyelf0801@gmail.com>
2023-04-07 19:32:33 +0300

    Second Commit

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,8 +1,46 @@
+
+
 public class Radio {
     private int currentVolume;
 
     private int numberStation;
+    private int maxVolume = 100;
+    private int minVolume = 0;
+    private int numberOfRadioStations = 10;
 
+    private int maxNumberStation = numberOfRadioStations - 1;
+    private int minNumberStation = 0;
+
+    public Radio(int numberOfRadioStations) {
+        this.numberOfRadioStations = numberOfRadioStations;
+        this.maxNumberStation = numberOfRadioStations - 1;
+
+
+    }
+
+    public Radio() {
+        this.numberOfRadioStations = getNumberOfRadioStations();
+    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
+
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+    public int getMaxNumberStation() {
+        return maxNumberStation;
+    }
+
+    public int getMinNumberStation() {
+        return minNumberStation;
+    }
+
+    public int getNumberOfRadioStations() {
+        return numberOfRadioStations;
+    }
 
     public int getCurrentVolume() {
         return currentVolume;
@@ -14,10 +52,10 @@ public class Radio {
     }
 
     public void setCurrentVolume(int newCurrentVolume) {
-        if (newCurrentVolume > 100) {
+        if (newCurrentVolume > maxVolume) {
             return;
         }
-        if (newCurrentVolume < 0) {
+        if (newCurrentVolume < minVolume) {
             return;
         }
         currentVolume = newCurrentVolume;
@@ -25,17 +63,17 @@ public class Radio {
     }
 
     public void setNumberStation(int newNumberStation) {
-        if (newNumberStation > 9) {
+        if (newNumberStation > maxNumberStation) {
             return;
         }
-        if (newNumberStation < 0) {
+        if (newNumberStation < minNumberStation) {
             return;
         }
         numberStation = newNumberStation;
     }
 
     public void switchStationUp() {
-        if (numberStation == 9) {
+        if (numberStation == maxNumberStation) {
             numberStation = 0;
         } else {
             numberStation = numberStation + 1;
@@ -44,8 +82,8 @@ public class Radio {
     }
 
     public void switchStationDown() {
-        if (numberStation == 0) {
-            numberStation = 9;
+        if (numberStation == minNumberStation) {
+            numberStation = maxNumberStation;
         } else {
             numberStation = numberStation - 1;
         }
@@ -55,7 +93,7 @@ public class Radio {
 
 
     public void increaseVolume() {
-        if (currentVolume < 100) {
+        if (currentVolume < maxVolume) {
             currentVolume = currentVolume + 1;
         }
 
@@ -63,7 +101,7 @@ public class Radio {
     }
 
     public void decreaseVolume() {
-        if (currentVolume > 0) {
+        if (currentVolume > minVolume) {
             currentVolume = currentVolume - 1;
 
 
